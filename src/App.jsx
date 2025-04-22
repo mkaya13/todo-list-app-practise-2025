da7c44b8c999cbc9bf013b1a492a81115a8f6f16
mkaya13 <mertkaya783@hotmail.com>
2025-04-22 14:25:35 +0300

    Push all the codes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './index.css'
+import ToDoList from './components/ToDoList/ToDoList.jsx'
+
+function App() {
+
+  return (
+    <>
+    <ToDoList />
+    </>
+  )
+}
+
+export default App
